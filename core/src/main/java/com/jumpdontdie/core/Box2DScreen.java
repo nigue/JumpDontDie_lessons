6ac8c752d8991be5fb9eab0b6d712b32a77fc671
nigue <anibaldavor@gmail.com>
2016-05-19 10:50:39 -0400

    avances

--- core/src/main/java/com/jumpdontdie/core/Box2DScreen.java
+++ core/src/main/java/com/jumpdontdie/core/Box2DScreen.java
@@ -2,6 +2,8 @@ package com.jumpdontdie.core;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+//import com.badlogic.gdx.physics.box2d.World;
 
 public class Box2DScreen extends BaseScreen {
 
@@ -9,17 +11,22 @@ public class Box2DScreen extends BaseScreen {
         super(mainGame);
     }
 
+//    private World world;
+    
     @Override
     public void show() {
+//        world = new World(new Vector2(0, -10), true);
     }
 
     @Override
     public void dispose() {
+//        world.dispose();
     }
 
     @Override
     public void render(float f) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        world.step(f, 6, 2);
     }
 
 }
